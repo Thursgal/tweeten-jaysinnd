095a376107bde46109716a0b61d2cd8209876cb9
Unknown <bo0842372@otc.edu>
2017-09-27 13:15:09 -0500

    Added content
    Added a first tweet, like button, and retweet button.

---
+++ like.js
@@ -0,0 +1,9 @@
+var btns = Array.from(document.getElementsByClassName("increment"));
+
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        let currentCount = btn.nextSibling.textContent.trim()
+        newCount = parseInt(currentCount) + 1
+        btn.nextSibling.textContent = newCount;
+    })
+})
